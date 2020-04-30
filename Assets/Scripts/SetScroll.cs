fbd2654cab39ca12f3e0436e0b5dc6a6966ff60c
Janik Mayr <jan.222.ik@gmail.com>
2020-04-30 13:30:49 +0200

    Made a scrollbar, duh

---
+++ Assets/Scripts/SetScroll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetScroll : MonoBehaviour
+{
+    public static SetScroll Instance;
+    public List<Scrollbar> scrollbars;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            setScrolling(1f);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            setScrolling(0.5f);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            setScrolling(0f);
+        }
+    }
+
+    public void setScrolling(float f)
+    {
+        f = Math.Max(0, Math.Min(1, f));
+        foreach (var scrollbar in scrollbars)
+        {
+            scrollbar.value = f;
+        }
+    }
+}
