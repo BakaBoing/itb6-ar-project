4a3371718a8a60756971a2507b420980f3728a85
BakaBoing <baka.boing@gmail.com>
2020-05-01 11:23:00 +0200

    rebased on dev branch

--- Assets/Scripts/PrinterTrackableEventHandler.cs
+++ Assets/Scripts/PrinterTrackableEventHandler.cs
@@ -1,12 +1,4 @@
-﻿/*==============================================================================
-Copyright (c) 2019 PTC Inc. All Rights Reserved.
-
-Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
-All Rights Reserved.
-Confidential and Proprietary - Protected under copyright and other laws.
-==============================================================================*/
-
-using System;
+﻿using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
